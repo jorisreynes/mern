c0e31d1492f5036cf5f26fb40aa2ce7bd31015e7
jorisreynes <joris.reynes81@gmail.com>
2023-10-14 15:47:59 +0200

    Find username

--- src/components/games-list.component.js
+++ src/components/games-list.component.js
@@ -6,6 +6,7 @@ export default class GamesList extends Component {
     super(props);
     this.state = {
       games: [],
+      mostFrequentUsername: "",
     };
   }
 
@@ -14,19 +15,49 @@ export default class GamesList extends Component {
       .get("http://127.0.0.1:5000/games/")
       .then((response) => {
         this.setState({ games: response.data });
-        console.log(response.data);
+
+        // Find the players's username
+        this.findPlayerUsername(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  findPlayerUsername(gamesData) {
+    // create an empty list
+    let allUsernames = [];
+
+    // For each game
+    for (const gameData of gamesData) {
+      // we put all usernames in the list
+      allUsernames.push(gameData.white, gameData.black);
+    }
+
+    // We sort the list
+    allUsernames.sort();
+
+    // frequency count for each username
+    const usernameFrequency = {};
+    allUsernames.forEach((username) => {
+      usernameFrequency[username] = (usernameFrequency[username] || 0) + 1;
+    });
+
+    // find the username that appears the most
+    const mostFrequentUsername = Object.keys(usernameFrequency).reduce((a, b) =>
+      usernameFrequency[a] > usernameFrequency[b] ? a : b
+    );
+
+    // we register the value of mostFrequentUsername
+    this.setState({ mostFrequentUsername });
+  }
+
   render() {
-    const { games } = this.state;
+    const { games, mostFrequentUsername } = this.state;
     return (
       <div className="main">
-        <p>You are on the Games List component</p>
-        <p>Number of games: {games.length}</p>
+        <p>Welcome {this.state.mostFrequentUsername}</p>
+        <p>Number of your games in the database: {games.length}</p>
       </div>
     );
   }
